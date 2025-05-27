5195e0d741dd67ee6946a33c1bb634cfc9b05642
Eric Rubio Poch <eric.rubio.poch@gmail.com>
2025-05-27 09:27:10 +0200

    Merge remote-tracking branch 'origin/david' into eric

--- backend/battleship/api/views.py
+++ backend/battleship/api/views.py
@@ -1,218 +1,191 @@
-# views.py
-from rest_framework import viewsets, status
+from rest_framework import viewsets, filters, status, permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
+from .serializers import PlayerSerializer, GameSerializer, UserSerializer
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+
+
+
 from .models import Player, Game, Board, Vessel, BoardVessel, Shot
 from .serializers import (
-    UserSerializer, PlayerSerializer, GameSerializer,
-    BoardSerializer, VesselSerializer, BoardVesselSerializer, ShotSerializer
+    UserSerializer,
+    PlayerSerializer,
+    GameSerializer,
+    BoardSerializer,
+    VesselSerializer,
+    BoardVesselSerializer,
+    ShotSerializer,
 )
 
 class UserViewSet(viewsets.ModelViewSet):
-    """
-    GET    /user/
-    POST   /user/
-    GET    /user/{id}/
-    PUT    /user/{id}/
-    PATCH  /user/{id}/
-    DELETE /user/{id}/
-    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['username', 'email']
+    permission_classes = [permissions.IsAdminUser]
+
 
 class PlayerViewSet(viewsets.ModelViewSet):
     """
-    GET    /players/
-    POST   /players/
-    GET    /players/{id}/
-    PUT    /players/{id}/
-    PATCH  /players/{id}/
-    DELETE /players/{id}/
+    Provides list, create, retrieve, update, destroy for Player.
     """
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
-    http_method_names = ['get', 'post', 'put', 'patch', 'delete']
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nickname']
+
 
 class GameViewSet(viewsets.ModelViewSet):
     """
-    GET    /games/
-    POST   /games/
-    GET    /games/{gid}/
-    PUT    /games/{gid}/
-    PATCH  /games/{gid}/
-    DELETE /games/{gid}/
-
-    GET    /games/{gid}/players/
-    POST   /games/{gid}/players/
-    GET    /games/{gid}/players/{pid}/
-    DELETE /games/{gid}/players/{pid}/
-
-    GET    /games/{gid}/players/{pid}/boards/
-    GET    /games/{gid}/players/{pid}/boards/{bid}/
-
-    GET    /games/{gid}/players/{pid}/vessels/
-    POST   /games/{gid}/players/{pid}/vessels/
-    GET    /games/{gid}/players/{pid}/vessels/{vid}/
-    PUT    /games/{gid}/players/{pid}/vessels/{vid}/
-    PATCH  /games/{gid}/players/{pid}/vessels/{vid}/
-    DELETE /games/{gid}/players/{pid}/vessels/{vid}/
-
-    GET    /games/{gid}/players/{pid}/shots/
-    POST   /games/{gid}/players/{pid}/shots/
-    GET    /games/{gid}/players/{pid}/shots/{sid}/
+    Provides list, create, retrieve, update, destroy for Game.
     """
     queryset = Game.objects.all()
     serializer_class = GameSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['phase']
+    ordering_fields = ['id']
 
     def perform_create(self, serializer):
+        # Placeholder: assign owner as first User's Player
         player = get_object_or_404(Player, user=User.objects.first())
         serializer.save(owner=player)
 
-    # Players nested
-    @action(detail=True, methods=['get', 'post'], url_path='players')
-    def players_list(self, request, gid=None):
+    @action(detail=True, methods=['get'], url_path='players/(?P<pid>[^/.]+)')
+    def retrieve_player(self, request, pk=None, pid=None):
         game = self.get_object()
+        player = get_object_or_404(Player, pk=pid)
+
+        if player not in game.players.all():
+            return Response({'detail': 'Player not in this game.'}, status=404)
+
+        serializer = PlayerSerializer(player)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['delete'], url_path='players/(?P<pid>[^/.]+)')
+    def remove_player(self, request, pk=None, pid=None):
+        game = get_object_or_404(Game, pk=pk)
+        player = get_object_or_404(Player, pk=pid)
+
+        if player in game.players.all():
+            game.players.remove(player)
+            return Response(status=204)
+        return Response({'detail': 'Player not in this game.'}, status=404)
+
+    @action(detail=True, methods=['get', 'post'], url_path='players/(?P<pid>[^/.]+)/vessels')
+    def player_vessels(self, request, pk=None, pid=None):
+        game = self.get_object()
+        player = get_object_or_404(Player, id=pid)
+        board = get_object_or_404(Board, game=game, player=player)
+
         if request.method == 'GET':
-            players = game.players.all()
-            serializer = PlayerSerializer(players, many=True)
+            vessels = BoardVessel.objects.filter(board=board)
+            serializer = BoardVesselSerializer(vessels, many=True)
             return Response(serializer.data)
-        pid = request.data.get('id')
-        player = get_object_or_404(Player, pk=pid)
-        game.players.add(player)
-        return Response(PlayerSerializer(player).data, status=status.HTTP_201_CREATED)
+        elif request.method == 'POST':
+            data = request.data.copy()
+            data['board'] = board.id
+            serializer = BoardVesselSerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return None
 
-    @action(detail=True, methods=['get', 'delete'], url_path=r'players/(?P<pid>[^/.]+)')
-    def player_detail(self, request, gid=None, pid=None):
+    @action(detail=True, methods=['get', 'put', 'patch', 'delete'],
+            url_path='players/(?P<pid>[^/.]+)/vessels/(?P<vid>[^/.]+)')
+    def player_vessel_detail(self, request, pk=None, pid=None, vid=None):
         game = self.get_object()
-        player = get_object_or_404(Player, pk=pid)
+        player = get_object_or_404(Player, id=pid)
+        board = get_object_or_404(Board, game=game, player=player)
+        vessel = get_object_or_404(BoardVessel, id=vid, board=board)
+
         if request.method == 'GET':
-            return Response(PlayerSerializer(player).data)
-        game.players.remove(player)
-        return Response(status=status.HTTP_204_NO_CONTENT)
+            serializer = BoardVesselSerializer(vessel)
+            return Response(serializer.data)
+        elif request.method in ['PUT', 'PATCH']:
+            serializer = BoardVesselSerializer(vessel, data=request.data, partial=(request.method == 'PATCH'))
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        elif request.method == 'DELETE':
+            vessel.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return None
 
-    # Boards nested
-    @action(detail=True, methods=['get'], url_path=r'players/(?P<pid>[^/.]+)/boards')
-    def player_boards(self, request, gid=None, pid=None):
+    @action(detail=True, methods=['get', 'post'], url_path='players/(?P<pid>[^/.]+)/shots')
+    def player_shots(self, request, pk=None, pid=None):
         game = self.get_object()
-        player = get_object_or_404(Player, pk=pid)
+        player = get_object_or_404(Player, id=pid)
+        if request.method == 'GET':
+            shots = Shot.objects.filter(game=game, player=player)
+            serializer = ShotSerializer(shots, many=True)
+            return Response(serializer.data)
+        elif request.method == 'POST':
+            data = request.data.copy()
+            data['game'] = game.id
+            data['player'] = player.id
+            serializer = ShotSerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return None
+
+    @action(detail=True, methods=['get'], url_path='players/(?P<pid>[^/.]+)/shots/(?P<sid>[^/.]+)')
+    def player_shot_detail(self, request, pk=None, pid=None, sid=None):
+        game = self.get_object()
+        player = get_object_or_404(Player, id=pid)
+        shot = get_object_or_404(Shot, id=sid, game=game, player=player)
+        serializer = ShotSerializer(shot)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['get'], url_path='players/(?P<pid>[^/.]+)/boards')
+    def player_boards(self, request, pk=None, pid=None):
+        game = self.get_object()
+        player = get_object_or_404(Player, id=pid)
         boards = Board.objects.filter(game=game, player=player)
         serializer = BoardSerializer(boards, many=True)
         return Response(serializer.data)
 
-    @action(detail=True, methods=['get'], url_path=r'players/(?P<pid>[^/.]+)/boards/(?P<bid>[^/.]+)')
-    def player_board(self, request, gid=None, pid=None, bid=None):
+    @action(detail=True, methods=['get'], url_path='players/(?P<pid>[^/.]+)/boards/(?P<bid>[^/.]+)')
+    def player_board_detail(self, request, pk=None, pid=None, bid=None):
         game = self.get_object()
-        player = get_object_or_404(Player, pk=pid)
-        board = get_object_or_404(Board, pk=bid, game=game, player=player)
-        return Response(BoardSerializer(board).data)
-
-    # Vessels nested
-    @action(detail=True, methods=['get', 'post'], url_path=r'players/(?P<pid>[^/.]+)/vessels')
-    def player_vessels(self, request, gid=None, pid=None):
-        game = self.get_object()
-        player = get_object_or_404(Player, pk=pid)
-        if request.method == 'GET':
-            bv_qs = BoardVessel.objects.filter(board__game=game, board__player=player)
-            return Response(BoardVesselSerializer(bv_qs, many=True).data)
-        serializer = BoardVesselSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        board = get_object_or_404(
-            Board, pk=request.data.get('board'), game=game, player=player
-        )
-        serializer.save(board=board)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    @action(detail=True, methods=['get', 'put', 'patch', 'delete'], url_path=r'players/(?P<pid>[^/.]+)/vessels/(?P<vid>[^/.]+)')
-    def player_vessel_detail(self, request, gid=None, pid=None, vid=None):
-        game = self.get_object()
-        player = get_object_or_404(Player, pk=pid)
-        bv = get_object_or_404(
-            BoardVessel, pk=vid, board__game=game, board__player=player
-        )
-        if request.method == 'GET':
-            return Response(BoardVesselSerializer(bv).data)
-        if request.method in ['PUT', 'PATCH']:
-            partial = request.method == 'PATCH'
-            serializer = BoardVesselSerializer(bv, data=request.data, partial=partial)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            return Response(serializer.data)
-        bv.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    # Shots nested
-    @action(detail=True, methods=['get', 'post'], url_path=r'players/(?P<pid>[^/.]+)/shots')
-    def player_shots(self, request, gid=None, pid=None):
-        game = self.get_object()
-        player = get_object_or_404(Player, pk=pid)
-        if request.method == 'GET':
-            shots = Shot.objects.filter(game=game, player=player)
-            return Response(ShotSerializer(shots, many=True).data)
-        serializer = ShotSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        board = get_object_or_404(
-            Board, pk=request.data.get('board'), game=game, player=player
-        )
-        serializer.save(game=game, player=player, board=board)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    @action(detail=True, methods=['get'], url_path=r'players/(?P<pid>[^/.]+)/shots/(?P<sid>[^/.]+)')
-    def player_shot(self, request, gid=None, pid=None, sid=None):
-        game = self.get_object()
-        player = get_object_or_404(Player, pk=pid)
-        shot = get_object_or_404(
-            Shot, pk=sid, game=game, player=player
-        )
-        return Response(ShotSerializer(shot).data)
+        player = get_object_or_404(Player, id=pid)
+        board = get_object_or_404(Board, id=bid, game=game, player=player)
+        serializer = BoardSerializer(board)
+        return Response(serializer.data)
 
 
 class BoardViewSet(viewsets.ModelViewSet):
     """
-    GET    /boards/
-    POST   /boards/
-    GET    /boards/{id}/
-    PUT    /boards/{id}/
-    PATCH  /boards/{id}/
-    DELETE /boards/{id}/
+    Provides CRUD for Board.
     """
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
 
+
 class VesselViewSet(viewsets.ModelViewSet):
     """
-    GET    /vessels/
-    POST   /vessels/
-    GET    /vessels/{id}/
-    PUT    /vessels/{id}/
-    PATCH  /vessels/{id}/
-    DELETE /vessels/{id}/
+    Provides CRUD for Vessel.
     """
     queryset = Vessel.objects.all()
     serializer_class = VesselSerializer
 
+
 class BoardVesselViewSet(viewsets.ModelViewSet):
     """
-    GET    /boardvessels/
-    POST   /boardvessels/
-    GET    /boardvessels/{id}/
-    PUT    /boardvessels/{id}/
-    PATCH  /boardvessels/{id}/
-    DELETE /boardvessels/{id}/
+    Provides CRUD for BoardVessel (placement of vessels on boards).
     """
     queryset = BoardVessel.objects.all()
     serializer_class = BoardVesselSerializer
 
+
 class ShotViewSet(viewsets.ModelViewSet):
     """
-    GET    /shots/
-    POST   /shots/
-    GET    /shots/{id}/
-    PUT    /shots/{id}/
-    PATCH  /shots/{id}/
-    DELETE /shots/{id}/
+    Provides CRUD for Shot (player shots on boards)...
     """
     queryset = Shot.objects.all()
     serializer_class = ShotSerializer
-
